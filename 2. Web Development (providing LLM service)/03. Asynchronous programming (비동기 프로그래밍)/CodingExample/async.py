5e387aea63db631e5d0a68ae0b536764223bfb5e
GyuJin Lee <gjlee99211@gmail.com>
2025-05-21 11:56:51 +0900

    async.py updated

--- 2. Web Development (providing LLM service)/03. Asynchronous programming (비동기 프로그래밍)/CodingExample/async.py
+++ 2. Web Development (providing LLM service)/03. Asynchronous programming (비동기 프로그래밍)/CodingExample/async.py
@@ -1,24 +1,30 @@
 import asyncio
 import time
 
+# Synchronous Programming
+# the task is completed in order. next task will be waited until the one task is finished
+
+# every function with 'def' initialization is working as synchronous
 def sync_func():
     print('start sync function')
-    time.sleep(2)
+    time.sleep(2) # whole system stops for 2 seconds          
     print('end of sync function')
 
+# Asynchronous Programming
+# the task is completed independently. If there is time delay, we can do different task
 async def async_func():
     print('async function start')
-    await asyncio.sleep(2)
+    await asyncio.sleep(2) # waits for two seconds but other tasks can be computed
     print('end of async function')
 
 async def main():
-    print('Sync program:')
-    sync_func()
-    sync_func()
-    print("Async program")
+    print('--Sync program-- ')
+    sync_func() # this runs
+    sync_func() # after previous task is completed, this runs
+    print("--Async program-- ")
     await asyncio.gather(
-        async_func(),
-        async_func()
+        async_func(), # this runs
+        async_func()  # when previous one awaits, this runs
     )
 
 asyncio.run(main())
